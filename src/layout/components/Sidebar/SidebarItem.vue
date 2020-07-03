bd6335d73d3bcad98c999bfd4f4c67fb2829e2e5
mengyb <mengyb@rsclouds.com>
2020-07-03 10:42:46 +0800

    修改一二级菜单显示样式及点击事件

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -1,16 +1,30 @@
 <template>
   <div v-if="!item.hidden">
-    <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
+    <template
+      v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow"
+    >
       <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path)">
-        <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-          <item :icon="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)" :title="onlyOneChild.meta.title" />
+        <el-menu-item
+          :index="resolvePath(onlyOneChild.path)"
+          :class="{'submenu-title-noDropdown':!isNest}"
+          @click="storeMenuid(onlyOneChild)"
+        >
+          <item
+            :icon="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)"
+            :title="onlyOneChild.meta.title"
+          />
         </el-menu-item>
       </app-link>
     </template>
 
     <el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
       <template slot="title">
-        <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title" />
+        <item
+          v-if="item.meta"
+          :icon="item.meta && item.meta.icon"
+          :title="item.meta.title"
+          @click="storeMenuid(item)"
+        />
       </template>
       <sidebar-item
         v-for="child in item.children"
@@ -30,6 +44,7 @@ import { isExternal } from '@/utils/validate'
 import Item from './Item'
 import AppLink from './Link'
 import FixiOSBug from './FixiOSBug'
+import global from '@/store/Globel'
 
 export default {
   name: 'SidebarItem',
@@ -75,7 +90,7 @@ export default {
 
       // Show parent if there are no child router to display
       if (showingChildren.length === 0) {
-        this.onlyOneChild = { ... parent, path: '', noShowingChildren: true }
+        this.onlyOneChild = { ...parent, path: '', noShowingChildren: true }
         return true
       }
 
@@ -89,6 +104,10 @@ export default {
         return this.basePath
       }
       return path.resolve(this.basePath, routePath)
+    },
+    storeMenuid(item) {
+      // store.globalvariables.menuId = menuId
+      global.secondMenuinfo = item
     }
   }
 }
---
+++ src/layout/components/SiderbarMenu.vue
@@ -0,0 +1,63 @@
+<template>
+  <ul class="siderBarright">
+    <li v-for="(item,index) in nav" :key="index" class="productImg" @click="selectNav(item.title)">
+      <img :src="isSelected ===item.title?item.url_one:item.url" alt="item.title">
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'SidebarMenu',
+  data() {
+    return {
+      isSelected: '首页',
+      nav: [
+        {
+          title: '首页',
+          url: require('@/assets/daohang/home.png'),
+          url_one: require('@/assets/daohang/home_h.png')
+        },
+        {
+          title: '天汇搜',
+          url: require('@/assets/daohang/THSearch.png'),
+          url_one: require('@/assets/daohang/THSearch.png')
+        },
+        {
+          title: '易搜',
+          url: require('@/assets/daohang/yi.png'),
+          url_one: require('@/assets/daohang/yi_h.png')
+        },
+        {
+          title: '数方',
+          url: require('@/assets/daohang/shuf.png'),
+          url_one: require('@/assets/daohang/shuf_h.png')
+        },
+        {
+          title: '天小惠',
+          url: require('@/assets/daohang/ai.png'),
+          url_one: require('@/assets/daohang/ai_h.png')
+        },
+        {
+          title: '地球引擎',
+          url: require('@/assets/daohang/earth.png'),
+          url_one: require('@/assets/daohang/earth_h.png')
+        },
+        {
+          title: '招聘',
+          url: require('@/assets/daohang/join.png'),
+          url_one: require('@/assets/daohang/join_h.png')
+        }
+      ]
+    }
+  },
+  methods: {
+    selectNav(title) {
+      this.isSelected = title
+    }
+  }
+}
+</script>
+
+<style>
+</style>
