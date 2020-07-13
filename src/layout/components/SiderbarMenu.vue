b9e8ed77581dea379b64262f959c88aaf271850f
mengyb <mengyb@rsclouds.com>
2020-07-13 10:14:39 +0800

    1、增加主页的跳转；2、删除招聘的按钮；3、切换天小惠和地球引擎的顺序；

--- src/layout/components/SiderbarMenu.vue
+++ src/layout/components/SiderbarMenu.vue
@@ -1,9 +1,12 @@
 <template>
   <ul class="siderBarright">
-    <li v-for="(item,index) in nav" :key="index" class="productImg" @click="selectNav(item.title)">
-      <img :src="isSelected ===item.title?item.url_one:item.url" alt="item.title">
+    <li v-for="(item,index) in nav" :key="index" class="productImg" @click="selectNav(item.title,item.website_url)">
+      <div :class="{'is-active':isSelected===item.title}">
+        <img :src="isSelected ===item.title?item.url_one:item.url" alt="item.title">
+      </div>
     </li>
   </ul>
+  <!-- class="productImg" -->
 </template>
 
 <script>
@@ -16,48 +19,55 @@ export default {
         {
           title: '首页',
           url: require('@/assets/daohang/home.png'),
-          url_one: require('@/assets/daohang/home_h.png')
+          url_one: require('@/assets/daohang/home_h.png'),
+          website_url: 'http://112.82.244.42/'
         },
         {
           title: '天汇搜',
           url: require('@/assets/daohang/THSearch.png'),
-          url_one: require('@/assets/daohang/THSearch_h.png')
+          url_one: require('@/assets/daohang/THSearch_h.png'),
+          website_url: '/home'
         },
         {
           title: '易搜',
           url: require('@/assets/daohang/yi.png'),
-          url_one: require('@/assets/daohang/yi_h.png')
+          url_one: require('@/assets/daohang/yi_h.png'),
+          website_url: ''
         },
         {
           title: '数方',
           url: require('@/assets/daohang/shuf.png'),
-          url_one: require('@/assets/daohang/shuf_h.png')
-        },
-        {
-          title: '天小惠',
-          url: require('@/assets/daohang/ai.png'),
-          url_one: require('@/assets/daohang/ai_h.png')
+          url_one: require('@/assets/daohang/shuf_h.png'),
+          website_url: ''
         },
         {
           title: '地球引擎',
           url: require('@/assets/daohang/earth.png'),
-          url_one: require('@/assets/daohang/earth_h.png')
+          url_one: require('@/assets/daohang/earth_h.png'),
+          website_url: ''
         },
         {
-          title: '招聘',
-          url: require('@/assets/daohang/join.png'),
-          url_one: require('@/assets/daohang/join_h.png')
+          title: '天小惠',
+          url: require('@/assets/daohang/ai.png'),
+          url_one: require('@/assets/daohang/ai_h.png'),
+          website_url: ''
         }
+        // {
+        //   title: '招聘',
+        //   url: require('@/assets/daohang/join.png'),
+        //   url_one: require('@/assets/daohang/join_h.png'),
+        //   website_url: ''
+        // }
       ]
     }
   },
   methods: {
-    selectNav(title) {
+    selectNav(title, url) {
       this.isSelected = title
+      if (url !== '') {
+        window.open(url, '_blank')
+      }
     }
   }
 }
 </script>
-
-<style>
-</style>
