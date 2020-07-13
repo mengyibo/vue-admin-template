bb5c0138204774f44cf438ec3551eb0c574c4624
mengyb <mengyb@rsclouds.com>
2020-07-13 10:05:01 +0800

    axios请求：1、查询推荐标签；2、搜索框查询接口；3、查询所有三级菜单；4、查询所有网站；

--- src/api/remote-search.js
+++ src/api/remote-search.js
@@ -12,3 +12,27 @@ export function searchWebsites(thirdMenuid) {
     method: 'get'
   })
 }
+export function searchRecommendation() {
+  return request({
+    url: '/tags?numCount=4',
+    method: 'get'
+  })
+}
+export function searchEngine(level3CatalogObjectId, searcText) {
+  return request({
+    url: '/search?searchMsg=' + searcText,
+    method: 'get'
+  })
+}
+export function searchallThirdmenu(selectedSecondmenu) {
+  return request({
+    url: '/level2SubWebsites/' + selectedSecondmenu,
+    method: 'get'
+  })
+}
+export function searchallwebsites() {
+  return request({
+    url: '/websites/all',
+    method: 'get'
+  })
+}
